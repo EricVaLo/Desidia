65217e1f036b782763203c4294cb96cc11369ff9
EricVL <da.evl.2405@gmail.com>
2025-08-25 14:32:38 -0500

    0.0.1
    Base

--- Scripts/Start.gd
+++ Scripts/Start.gd
@@ -3,3 +3,7 @@ class_name Main_menu
 
 func _on_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_iniciar_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Niveles.tscn") # Replace with function body.
